96b40dedd13ae17a6d1099e782ca043dfd1e002c
Niall Guerin <niallguerin@nialls-mbp.home>
2022-10-13 09:02:40 +0100

    fix for negative value in unit test

--- src/02-module/test_sml/test_sml.py
+++ src/02-module/test_sml/test_sml.py
@@ -19,7 +19,7 @@ def test_generate_atm_withdrawal(credit_card_number: str, cash_amounts: list, le
 
 # add new unit test exercise as optional task for homework for week 2 lab
 def test_time_delta():
-    date_time1 = int((datetime(2020, 5, 17, 10, 35, 00).strftime("%Y%m%d%H%M%S")))
-    date_time2 = int((datetime(2020, 5, 20, 15, 45, 00).strftime("%Y%m%d%H%M%S")))
-    expected_time_delta = -3051000
+    date_time1 = int((datetime(2020, 5, 25, 11, 35, 00).strftime("%Y%m%d%H%M%S")))
+    date_time2 = int((datetime(2020, 5, 20, 15, 40, 00).strftime("%Y%m%d%H%M%S")))
+    expected_time_delta = 4959500
     assert(cc_features.time_delta(date_time1, date_time2)) == expected_time_delta
